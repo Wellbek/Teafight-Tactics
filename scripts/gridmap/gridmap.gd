172892932336a16945f32f32ba5ef6eba6ce263b
Wellbek <louis.wellmeyer@gmail.com>
2024-03-17 11:40:57 +0100

    Unit grid

--- scripts/gridmap/gridmap.gd
+++ scripts/gridmap/gridmap.gd
@@ -3,6 +3,10 @@ extends Node3D
 @export var x: int = 7
 @export var z: int = 4
 
+@export var type: Type = Type.SQUARE
+
+enum Type {SQUARE, HEX}
+
 #https://www.redblobgames.com/grids/hexagons/
 const size = 1
 const w = sqrt(3) * size
@@ -14,15 +18,26 @@ const hCorrection = .8
 const wCorrection = 1.1
 
 func _ready():
-	generateHexGrid(x,z)
+	if type == Type.SQUARE: generateGrid(x,z)
+	elif type == Type.HEX: generateHexGrid(x,z)
+	else: push_error("Unknown grid type")
 
 func generateHexGrid(_x: int, _z:int):
 	for xi in _x:
 		for zi in _z:
-			var hex = preload("res://src/hextile/hex.tscn").instantiate()
+			var hex = preload("res://src/tiles/hex.tscn").instantiate()
 			add_child(hex)
 			var hexX = w*xi*wCorrection
 			var hexZ = hCorrection*h*zi
 			if zi % 2 != 0:
 				hexX += offset
-			hex.global_transform.origin = Vector3(hexX, 0, hexZ)
+			hex.transform.origin = Vector3(hexX, 0, hexZ)
+			
+func generateGrid(_x: int, _z:int):
+	for xi in _x:
+		for zi in _z:
+			var square = preload("res://src/tiles/square.tscn").instantiate()
+			add_child(square)
+			var squareX = w*xi
+			var squareZ = h*zi
+			square.transform.origin = Vector3(squareX, 0, squareZ)
