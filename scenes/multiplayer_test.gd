cb56180589f1f51b304acf5e96b5b6c33f9d23e4
Wellbek <louis.wellmeyer@gmail.com>
2024-03-25 13:22:06 +0100

    Multiplayer system now able to spawn and sync peer boards as intended; Note: Code needs refactoring and a lot was/is temporarily implemented just to see things working

--- scenes/multiplayer_test.gd
+++ scenes/multiplayer_test.gd
@@ -3,26 +3,51 @@ extends Control
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
 
+@export_subgroup("Temporary GUI Stuff")
+@export var startButton: Button
+@export var enable_on_start: Array[Control]
+@export var disable_on_start: Array[Control]
+
+func _ready():
+	for c in enable_on_start:
+		c.visible = false
+	startButton.visible = false
+
 func _on_tmp_host_pressed():
 	peer.create_server(135, 8)
 	multiplayer.multiplayer_peer = peer
-	_add_player()
+	disable_enable()
 
 func _on_tmp_join_pressed():
-	peer.create_client("localhost", 135)
+	peer.create_client("127.0.0.1", 135)
 	multiplayer.multiplayer_peer = peer
+	disable_enable()
 
-func _add_player(id = 1):
+func add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
-	get_tree().root.get_child(0).find_child("World").call_deferred("add_child", player)
+	get_tree().root.get_child(0).find_child("World").call_deferred("add_child", player, true)
+			
+func start_game():
+	if not multiplayer.is_server(): return
+	
+	startButton.visible = false
+	
+	for id in multiplayer.get_peers():
+		add_player(id)
+	
+	if not OS.has_feature("dedicated_server"):
+		add_player()
+		
+	#print(get_tree().root.get_child(0).find_child("World").get_children())
+	
+func disable_enable():
+	for c in enable_on_start:
+		c.visible = true
+	for c in disable_on_start:
+		c.visible = false
+	
+	if multiplayer.is_server(): startButton.visible = true
 
-func _on_tmp_test_pressed():
-	print(multiplayer.get_peers())
-	print(get_tree().root.get_child(0).find_child("World").get_children())
-
-func _on_multiplayer_spawner_spawned(node):
-	node.global_transform.origin.x += 18 * (multiplayer.get_peers().size())
-	var newMaterial = StandardMaterial3D.new()
-	newMaterial.albedo_color = Color.BROWN
-	node.get_child(1).get_child(1).set_surface_override_material(0, newMaterial)
+func _on_tmp_start_game_pressed():
+	start_game()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,14 +3,29 @@ extends Node3D
 var main
 
 @export var unitGrid: Node3D
+@export var camera: Camera3D
 
 var units = []
 
+var myid
+
 func _enter_tree():
 	main = get_tree().root.get_child(0)
-	set_multiplayer_authority(multiplayer.get_unique_id())
-	print(is_multiplayer_authority())
 	main.setPlayer(self)
+	
+func _ready():
+	print(name)
+	myid = name.to_int()
+	set_multiplayer_authority(myid)
+	
+	var ids = multiplayer.get_peers()
+	ids.append(myid)
+	ids.sort()
+	var i = ids.find(myid)
+	
+	if is_multiplayer_authority():
+		global_transform.origin.x += 19 * i
+		camera.current = true
 
 func appendUnit(unit):
 	units.append(unit)
